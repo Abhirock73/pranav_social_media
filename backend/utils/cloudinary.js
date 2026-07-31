23dbe5ce94430d1d52080bfddeed5cb2560660de
162730128 <sahabhi1234567@gmail.com>
2026-07-31 17:53:36 +0000

    Update backend/utils/cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,7 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config({});
-
+ added new line'
+ 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
